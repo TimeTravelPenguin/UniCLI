f4c8bb2eee66c75832d0099d83a2b365a4da25a7
Phillip <FilupErwinSmith@gmail.com>
2022-06-05 23:26:31 +1000

    initial push

---
+++ UniCLI/CommandLine.fs
@@ -0,0 +1,28 @@
+﻿module UniCLI.CommandLine
+
+open Argu
+
+type Configuration = { Configs: ConfigKind list }
+and ConfigKind = { Name: string; TemplateRoot: string }
+
+type ProjectKind =
+    | LatexAssignment
+    | CourseDirectory
+
+type ProjectOperation =
+    | Create
+    | Update
+
+type CliArguments =
+    | [<Unique; EqualsAssignment; AltCommandLine("-op")>] ProjectOperation of ProjectOperation
+    | [<Mandatory; Last; Unique; EqualsAssignment; AltCommandLine("-p")>] Path of string
+    | [<Unique; EqualsAssignment; AltCommandLine("-template")>] ProjectTemplate of ProjectKind
+    | [<NoCommandLine>] TemplatesRoot of string
+
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | ProjectOperation _ -> "specify whether to create a new project or update an existing one."
+            | Path _ -> "specify the path of a new project, or the root of an existing one."
+            | ProjectTemplate _ -> "specify the project template to use."
+            | TemplatesRoot _ -> "the root directory of all project templates"
---
+++ UniCLI/Program.fs
@@ -0,0 +1,77 @@
+﻿module UniCli.Program
+
+open System
+open System.IO
+open System.Reflection
+open Argu
+open UniCLI.CommandLine
+
+let parser =
+    let errorHandler =
+        ProcessExiter(
+            colorizer =
+                function
+                | ErrorCode.HelpText -> Some ConsoleColor.DarkYellow
+                | _ -> Some ConsoleColor.Red
+        )
+
+    ArgumentParser.Create<CliArguments>(programName = "UniCli.exe", errorHandler = errorHandler)
+
+let appSettingPath =
+    let root =
+        Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
+
+    Path.Combine(root, "appSettings.config")
+
+let appSettingExist =
+    File.Exists(appSettingPath)
+
+let createAppSettings () =
+    printfn
+        $"Created `appSettings.config`.{Environment.NewLine}Please edit to template root value before rerunning the application."
+
+    let contents =
+        parser.PrintAppSettingsArguments [ TemplatesRoot "path/to/templates/here" ]
+
+    File.WriteAllText(appSettingPath, contents)
+
+let exec (operation: ProjectOperation) (kind: ProjectKind) (templatesPath: string) (path: string) =
+    // TODO
+    ()
+
+let parse (results: ParseResults<CliArguments>) =
+    let path = results.GetResult Path
+
+    let op =
+        match results.TryGetResult ProjectOperation with
+        | Some operation -> operation
+        | None -> ProjectOperation.Create
+
+    let projKind =
+        match results.TryGetResult ProjectTemplate with
+        | Some kind -> kind
+        | None -> ProjectKind.LatexAssignment
+
+    let templatesRoot =
+        results.GetResult TemplatesRoot
+
+    exec op projKind templatesRoot path
+
+
+[<EntryPoint>]
+let main args =
+    if not appSettingExist then
+        createAppSettings ()
+    else
+        try
+            let reader =
+                ConfigurationReader.FromAppSettingsFile(appSettingPath)
+
+            let results =
+                parser.Parse(inputs = args, raiseOnUsage = true, configurationReader = reader)
+
+            parse results |> ignore
+        with
+        | e -> printfn $"{e.Message}"
+
+    0
